ec989c95cdadc846baadd935350a0ee4792981dc
FatalError <ripcode0@gmail.com>
2024-07-11 09:53:29 +0900

    up

--- d3d12/context.cpp
+++ d3d12/context.cpp
@@ -7,6 +7,8 @@ d3d12_context::d3d12_context(int cx, int cy, HWND hwnd)
     
     create_command_queue();
 
+    create_swapchain(cx, cy);
+
 }
 
 void d3d12_context::create_device() 
@@ -28,6 +30,8 @@ void d3d12_context::create_device()
     ComPtr<IDXGIFactory4> factory;
     HR(CreateDXGIFactory2(factory_flags, IID_PPV_ARGS(&factory)));
 
+    HR(factory.As(&m_factory));
+
     ComPtr<IDXGIAdapter> warp_adapter;
     HR(factory->EnumWarpAdapter(IID_PPV_ARGS(&warp_adapter)));
 
@@ -46,5 +50,49 @@ void d3d12_context::create_command_queue()
 
     HR(m_device->CreateCommandQueue(&desc, IID_PPV_ARGS(&m_command_queue)));
 
+}
 
+void d3d12_context::create_swapchain(int cx, int cy) 
+{
+    DXGI_SWAP_CHAIN_DESC1 desc{};
+    desc.BufferCount = frame_buffer_count;
+    desc.Width = cx;
+    desc.Height = cy;
+    desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+    desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    desc.SampleDesc.Count = 1;
+
+    ComPtr<IDXGISwapChain1> swapchain;
+    HR(m_factory->CreateSwapChainForHwnd(
+        m_command_queue.Get(),
+        m_hwnd,
+        &desc,
+        nullptr,
+        nullptr,
+        &swapchain
+    ));
+
+    HR(m_factory->MakeWindowAssociation(m_hwnd, DXGI_MWA_NO_ALT_ENTER));
+    HR(swapchain.As(&m_swapchain));
+
+    m_frame_index = m_swapchain->GetCurrentBackBufferIndex();
+}
+
+void d3d12_context::create_descriptor_heaps() 
+{
+    D3D12_DESCRIPTOR_HEAP_DESC rtv_desc{};
+    rtv_desc.NumDescriptors = frame_buffer_count;
+    rtv_desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+    rtv_desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+    
+    HR(m_device->CreateDescriptorHeap(&rtv_desc, IID_PPV_ARGS(&m_rtv_heap)));
+
+    D3D12_DESCRIPTOR_HEAP_DESC cbv_desc{};
+    cbv_desc.NumDescriptors = 1;
+    cbv_desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+    cbv_desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+    HR(m_device->CreateDescriptorHeap(&cbv_desc, IID_PPV_ARGS(&m_cbv_heap)));
+
+    
 }
--- d3d12/context.h
+++ d3d12/context.h
@@ -2,18 +2,33 @@
 
 #include "config.h"
 
+#define frame_buffer_count 2
+
+
 class d3d12_context
 {
 public:
     d3d12_context(int cx, int cy, HWND hwnd);
+
     void create_device();
     void create_command_queue();
+    void create_swapchain(int cx, int cy);
+    void create_descriptor_heaps();
 
     ComPtr<ID3D12Device> m_device;
     ComPtr<ID3D12CommandQueue> m_command_queue;
 
-    ComPtr<IDXGISwapChain> m_swapchain;
+    ComPtr<IDXGISwapChain3> m_swapchain;
+    
+    ComPtr<IDXGIFactory4> m_factory;
+
+    ComPtr<ID3D12DescriptorHeap> m_rtv_heap;
+    ComPtr<ID3D12DescriptorHeap> m_cbv_heap;
+
+    ComPtr<ID3D12Resource> m_render_targets[frame_buffer_count];
 
     HWND m_hwnd;
+    uint m_frame_index;
+
 };
 
